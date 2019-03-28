7bef2389d3eed08ff14168b6b763c77107dddfe6
Aditya Chaturvedi <39881830+aditya2912@users.noreply.github.com>
2019-03-28 19:47:00 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,9 +1,18 @@
 
 import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-
+import App from './App';
 function Index() {
   return <h2> Home </h2>;
+}
+
+function AnotherDummyPageView() {
+  
+  return (
+    <div>
+      <input type = "text" value = {this.state.userName} onChange = {this.handleUserNameChange} />
+    </div>
+  )
 }
 
 function AppRouter() {
@@ -15,9 +24,13 @@ function AppRouter() {
            <li>
            <Link to="/dummyPage"> Dummy Page </Link>
            </li>
+           <li>
+           <Link to ="/anotherDummyPage"> Another Dummy Page </Link>
+           </li>
            </ul>
            </nav>
         <Route path="/dummyPage" exact component={Index} />
+        <Route path="/anotherDummyPage" exact component={AnotherDummyPageView} />
       </div>
     </Router>
   );
