746d4411c98f4b533e0199d0154a6dde73299a5f
Aditya Chaturvedi <39881830+aditya2912@users.noreply.github.com>
2019-05-14 20:19:43 +0530

    Update jumpingGame.py

--- jumpingGame.py
+++ jumpingGame.py
@@ -1,6 +1,8 @@
 import turtle
 import time
+import threading
 import random
+from multiprocessing  import Pool
 
 delay = 0.04
 obstacle_delay = 0.02
@@ -103,13 +105,13 @@ def move_road_left_lines():
         y_coordinate_for_left_line2 = road_left_line2.ycor()
         road_left_line2.sety(y_coordinate_for_left_line2 - 20)
         time.sleep(delay)
-        
+
     if road_left_line.ycor() <= -290:
         road_left_line.sety(290)
-        
+
     if road_left_line2.ycor() <= -290:
         road_left_line2.sety(290)
-        
+
 def move_road_right_lines():
     if road_right_line2.direction == "down" and road_right_line.direction == "down":
         y_cor_right_line = road_right_line.ycor()
@@ -117,62 +119,72 @@ def move_road_right_lines():
         y_cor_right_line2 = road_right_line2.ycor()
         road_right_line2.sety(y_cor_right_line2 - 20)
         time.sleep(delay)
-        
+
     if road_right_line.ycor() <= -290:
         road_right_line.sety(290)
-        
+
     if road_right_line2.ycor() <= -290:
         road_right_line2.sety(290)
-        
+
 def move_road_middle_lines():
     if road_middle_line.direction == "down" and road_middle_line2.direction == "down":
         y_cor_for_mid_line = road_middle_line.ycor()
         road_middle_line.sety(y_cor_for_mid_line - 20)
-        
+
         y_cor_for_mid_line2 = road_middle_line2.ycor()
         road_middle_line2.sety(y_cor_for_mid_line2 - 20)
-        
+
     if road_middle_line.ycor() <= -290:
         road_middle_line.sety(290)
-        
+
     if road_middle_line2.ycor() <= -290:
         road_middle_line2.sety(290)
 
-
-def move_obstacles():
-    if obstacle.direction == "down" and obstacle2.direction == "down" and obstacle.direction == "down":
+def move_obstacle1():
+    global obstacle_delay
+    if obstacle.direction == "down":
         y = obstacle.ycor()
         obstacle.sety(y - 20)
-        time.sleep(obstacle_delay)
-        
+        # time.sleep(obstacle_delay)
+
+    if obstacle.ycor() <= -290:
+        x = random.randint(-150, 150)
+        print(x)
+        obstacle.goto(x, 290)
+
+
+def move_obstacle2():
+    global obstacle2_delay
+    if obstacle2.direction == "down":
         y2 = obstacle2.ycor()
         obstacle2.sety(y2 - 20)
-#        
+        # time.sleep(obstacle2_delay)
+
+    if obstacle2.ycor() <=-290:
+           x2 = random.randint(-150, 150)
+           print(x2)
+           obstacle2.goto(x2, 290)
+
+def move_obstacle3():
+    global obstacle3_delay
+    if obstacle3.direction == "down":
         y3 = obstacle3.ycor()
         obstacle3.sety(y3 - 20)
-        
-        if obstacle.ycor() <= -290:
-            x = random.randint(-150, 150)
-            print(x)
-            obstacle.goto(x, 290)
-            
-        if obstacle2.ycor() <=-290:
-            x2 = random.randint(-150, 150)
-            print(x2)
-            obstacle2.goto(x2, 290)
-            
-        if obstacle3.ycor() <= -290:
-            x3 = random.randint(-150, 150)
-            print(x3)
-            obstacle3.goto(x3, 290)
-        
+        # time.sleep(obstacle3_delay)
+
+    if obstacle3.ycor() <= -290:
+        x3 = random.randint(-150, 150)
+        print(x3)
+        obstacle3.goto(x3, 290)
+
+
 def go_left():
     x_coordinate = car.xcor()
     if car.xcor() <= -280:
         car.setx(x_coordinate)
     else:
         car.setx(x_coordinate - 30)
-    
+
 def go_right():
     x_coordinate = car.xcor()
     if car.xcor() >= 280:
@@ -180,14 +192,67 @@ def go_right():
     else:
         car.setx(x_coordinate + 30)
 
+def restart_game():
+    road_left_line.direction = "down"
+    road_left_line2.direction = "down"
+    road_middle_line.direction = "down"
+    road_middle_line2.direction ="down"
+    road_right_line.direction = "down"
+    road_right_line.direction = "down"
+    obstacle.direction = "down"
+    obstacle2.direction = "down"
+    obstacle3.direction = "down"
+
+def check_for_collisions():
+    if obstacle.distance(car) < 20 or obstacle2.distance(car) < 20 or obstacle3.distance(car) < 20:
+        car.goto(-130, -260)
+        time.sleep(1)
+        road_left_line.goto(-250, 150)
+        road_left_line2.goto(-250, -120)
+        road_middle_line.goto(0, 150)
+        road_middle_line2.goto(0, -120)
+        road_right_line.goto(250, 250)
+        road_right_line2.goto(250, -120)
+        obstacle.goto(-130, 290)
+        obstacle2.goto(130, 290)
+        obstacle3.goto(-170, 290)
+        road_left_line.direction = "stop"
+        road_left_line2.direction = "stop"
+        road_middle_line.direction = "stop"
+        road_middle_line2.direction = "stop"
+        road_right_line.direction = "stop"
+        road_right_line2.direction = "stop"
+        obstacle.direction = "stop"
+        obstacle2.direction = "stop"
+        obstacle3.direction = "stop"
+        car.penup()
+
+def list_of_all_functions():
+    move_obstacle1()
+    move_obstacle2()
+    move_obstacle3()
+
 window.listen()
 window.onkey(go_left, "Left")
 window.onkey(go_right, "Right")
+window.onkey(restart_game, "Return")
+window.onkey(restart_game, "Up")
+window.onkey(restart_game, "Down")
 while True:
     window.update()
     move_road_left_lines()
     move_road_right_lines()
     move_road_middle_lines()
-    move_obstacles()
-    
+    if __name__ == '__main__':
+        pool = Pool(processes = 3)
+        result = pool.apply_async(move_obstacle1(), [], callback)
+    # obstacle_thread = threading.Thread(target = move_obstacle1(), args = ())
+    # obstacle_thread.start()
+    # time.sleep(obstacle_delay)
+    # obstacle2_thread = threading.Thread(target = move_obstacle2(), args =())
+    # obstacle3_thread = threading.Thread(target = move_obstacle3(), args = ())
+    # obstacle2_thread.start()
+    # obstacle3_thread.start()
+    check_for_collisions()
+
 window.mainloop()
